90242f3021f0ad3cf8492e0a30521717bdb43685
Paige Anderson <panderson.pdx@gmail.com>
2023-09-14 15:40:14 -0500

    first commit

---
+++ src/test/java/edu/tcu/cs/ewallet/EwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.ewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
